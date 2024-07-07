a0d57106f2f9f3445021e36676ccb2a1f3164040
vitaum <gvictor567@gmail.com>
2024-07-06 21:39:48 -0300

    fix: return the changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import About from './pages/About.tsx';
-import Replacement from './pages/Replacement.tsx';
-import Policy from './pages/Policy.tsx';
 
-const router = createBrowserRouter([
-  { path: "/", element: <About /> },
-  { path: "/troca", element: <Replacement /> },
-  { path: "/politica", element: <Policy /> },
-  { path: "*", element: <App /> },
-  { path: "*", element: <App /> },
-  { path: "*", element: <App /> },
-]);
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
